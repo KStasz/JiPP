2c78d12a3c974e30502e91ba5dadf43e9e34fde0
KStasz <krzysztofstaszek@hotmail.com>
2020-05-13 23:17:07 +0200

    Zadanie6

--- JiPP/Program.cs
+++ JiPP/Program.cs
@@ -1,19 +1,21 @@
-﻿using System;
+using System;
 using JiPP.Zadanie1;
 using JiPP.Zadanie2;
 using JiPP.Zadanie4;
 using JiPP.Zadanie5;
-
+using JiPP.Zadanie6;
 namespace JiPP
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             //AStartClass aStart = new AStartClass();
             //BStartClass bStartClass = new BStartClass();
             //CStartClass cStartClass = new CStartClass();
-            DStartClass dStartClass = new DStartClass();
+            //DStartClass dStartClass = new DStartClass();
+            EStartClass eStartClass = new EStartClass();
         }
     }
 }
---
+++ JiPP/Zadanie6/EStartClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JiPP.Zadanie6
+{
+    public class EStartClass
+    {
+        public EStartClass()
+        {
+            List<double> collection = new List<double>();
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write("Wprowadź liczbę nr." + i + ": ");
+                bool result = double.TryParse(Console.ReadLine(), out double number);
+                collection.Add(number);
+            }
+
+            Console.WriteLine("Średnia wynosi: " + avg(collection));
+            Console.WriteLine("Mediana wynosi: " + median(collection));
+
+
+
+        }
+
+        private double median(List<double> collection)
+        {
+            if (collection.Count % 2 != 0)
+            {
+                return collection[(collection.Count / 2) + 1];
+            }
+            else
+            {
+               return (collection[(collection.Count / 2) - 1] + collection[collection.Count / 2]) / 2;
+            }
+        }
+
+        private double avg(List<double> locallist)
+        {
+            double sum = 0;
+            foreach (var item in locallist)
+            {
+                sum += item;
+            }
+            return sum / locallist.Count;
+        }
+    }
+}
